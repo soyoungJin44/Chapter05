81214f9b30b90116ad1ec2ba7de4779578e15ed9
soyoungJin44 <wlsthdud0412@naver.com>
2024-07-15 11:54:36 +0900

    텍스트 테스트

---
+++ src/com/javaex/ex04/Ex01.java
@@ -0,0 +1,30 @@
+package com.javaex.ex04;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+
+public class Ex01 {
+
+	public static void main(String[] args) throws IOException {
+		
+		Writer fw = new FileWriter("C:\\javaStudy\\song.txt");
+		BufferedWriter bw = new BufferedWriter(fw);
+		
+		bw.write("학교종이 땡땡땡");
+		bw.newLine();  //줄바꿈
+		bw.write("어서 모이자");
+		bw.newLine();
+		bw.write("선생님이 우리를");
+		bw.newLine();
+		bw.write("기다린당~");
+		
+		
+		
+		
+		bw.close();
+		
+	}
+	
+}
